01a6e6d743a77815c65543eb243112f51c30129c
Cort Ferguson <98cort@gmail.com>
2016-05-23 20:52:07 +0000

    Adding simple form and some links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Quotalicious::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'quotes#index'
 
+  resources :quotes
+  # The priority is based upon order of creation: first created -> highest priority.
+
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
