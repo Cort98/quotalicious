51c7eb50e269d2c118388ff1460a7d7306753996
Cort Ferguson <98cort@gmail.com>
2016-05-23 14:14:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quotalicious::Application.config.secret_key_base = '346301f401d2087ecce3e4e70fdf5ba42b3e8938a79a0ca59aea13073032892714fc8e05be4fca1c41da6228b57460c9f14ed05a5f950181cde4fa215dac81d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quotalicious::Application.config.session_store :cookie_store, key: '_quotalicious_session'
